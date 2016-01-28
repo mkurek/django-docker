3e8fcdf5758fa7e7203babf346a1b8e307c02164
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-01-28 12:47:14 +0100

    wip

--- docker/init.sh
+++ docker/init.sh
@@ -4,3 +4,8 @@ sleep 10  # give db some time to run
 python3 manage.py migrate --noinput
 python3 manage.py loaddata blog/blog/fixtures/initial_data.json
 python3 manage.py collectstatic --noinput
+
+# pass the error (ex. when user already exist);
+# by muting the error, this script could be use to upgrade data container too
+python3 manage.py createsuperuser --noinput --username admin || true
+python3 ./docker/createsuperuser.py
